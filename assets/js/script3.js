35207d5fb8896d78f5d1a80fbfe1e2cbd624e38e
milo2112 <mjrp.ros@gmail.com>
2022-06-18 23:31:47 -0400

    Elimina console.log del código de función

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,6 +1,5 @@
 function operation() {
 
-    console.log("Ingresando a la función...");
     // capturamos dato ingresado a través del DOM
     var val1 = document.querySelector('#number1');
     var val2 = document.querySelector('#number2');
@@ -8,8 +7,7 @@ function operation() {
     
     // Realizamos concatenación
     var passw = val1.value + val2.value + val3.value;
-    console.log("password : " + passw);
-
+  
     // Realizamos bifurcación que despliega mensaje según password 
     if (passw == "911") {
         message.innerHTML = "Password 1 es " + passw + " y es correcta!";
