fa372bf63164857adcd7e9618e3cafbdd65fc8e7
milo2112 <mjrp.ros@gmail.com>
2022-06-18 23:30:34 -0400

    elimina console.log

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,6 +1,4 @@
 function imgBorder() {
-    console.log("ingresando...");
-
     // Buscamos las propiedades de la imagen en el DOM
     ImgColor = document.querySelector('#image').style.borderColor;
     ImgWidth = document.querySelector('#image').style.borderWidth;
@@ -10,11 +8,8 @@ function imgBorder() {
         image.style.borderColor = "red";
         image.style.borderWidth = "2px";
         image.style.borderStyle = "solid"; 
-        console.log("pinto el borde rojo de 2px...");
     } else {
         image.style.borderColor = 'white';
         image.style.borderWidth = "0px";
-        console.log("Le quito borde rojo de 2px...");
     }
-    console.log("voy saliendo de la funcion...");
 }
